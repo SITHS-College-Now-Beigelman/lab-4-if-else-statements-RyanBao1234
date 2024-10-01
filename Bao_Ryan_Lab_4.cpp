649d8881d7d3cb159dbc2a415c8b6601b0af49c3
RyanBao1234 <ryanb272@nycstudents.net>
2024-10-01 17:21:35 -0400

    Lab_4 Final
    FINALLY DONE

--- Bao_Ryan_Lab_4.cpp
+++ Bao_Ryan_Lab_4.cpp
@@ -10,21 +10,24 @@ int main()                                                //initialize main
 {
     string fruit;                                    //initialize variables
     float sugar;
+    int repeat;
+    repeat = 1;
+    int i = 1;
+
+    while (repeat == 1)
+    {
+        cout << "Please print the fruit name followed by a space and then the number of grams of sugar in a cup of that fruit" << endl;
+        cin >> fruit >> sugar;
+        if (sugar < 10)
+            cout << fruit << " is a LOW SUGAR FRUIT." << endl;
+        if (sugar >= 10 && sugar <= 14)
+            cout << fruit << " is a MEDIUM SUGAR FRUIT." << endl;
+        if (sugar > 14)
+            cout << fruit << " is a HIGH SUGAR FRUIT." << endl;
+            cout << endl;
+        cout << "Do you want to continue? (1 for Yes and 0 or No)" << endl;
+        cin >> repeat;
+    }
     
-    cout << endl;
-    cout << "Please print the fruit name followed by a space and then the number of grams of sugar in a cup of that fruit" << endl;
-    cin >> fruit >> sugar;
-    cout << endl;
-
-    if (sugar < 10)
-        cout << fruit << " is a LOW SUGAR FRUIT." << endl;
-
-    if (sugar >= 10 && sugar <= 14)
-        cout << fruit << " is a MEDIUM SUGAR FRUIT." << endl;
-
-    if (sugar > 14)
-        cout << fruit << " is a HIGH SUGAR FRUIT." << endl;
-    cout << endl;
-
 return 0;
-}
+} 
